b9f6989e367d82114102601a51fc428f50429b2a
Harish <harishdm2516@gmail.com>
2026-07-11 08:34:01 +0530

    flask

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ def home():
 
 @app.route("/hello")
 def hello():
-    return "Hello Hari! 🚀"
-
+    return "HELLO"
+@app.route("/hi")
+def hi():
+    return "HI"
 if __name__ == "__main__":
     app.run(debug=True)
